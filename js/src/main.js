3b02083fbada10583c69a93fb11b202f7b2d8c3a
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-03-27 02:43:40 -0700

    Adding properties to the components.

--- js/src/main.js
+++ js/src/main.js
@@ -5,15 +5,15 @@
 var CommentBox = React.createClass({
     render: function() {
         return (
-            <h1>Hello!!</h1>
+            <h1>Hello {this.props.name}!!</h1>
         );
     }
 });
 ReactDOM.render(
     <div>
-        <CommentBox />
-        <CommentBox />
-        <CommentBox />
+        <CommentBox name="React"/>
+        <CommentBox name="JSX"/>
+        <CommentBox name="World"/>
     </div>,
     document.getElementById('example')
 );
