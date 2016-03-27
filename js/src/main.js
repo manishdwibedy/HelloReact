34b111c407c3f2a74608dd63479b9e47896a3ce5
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-03-27 02:38:20 -0700

    Creating a component and rendering it.

--- js/src/main.js
+++ js/src/main.js
@@ -2,7 +2,14 @@
  * Created by manishdwibedy on 3/27/16.
  */
 
+var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <h1>Hello!!</h1>
+        );
+    }
+});
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <CommentBox />,
     document.getElementById('example')
 );
